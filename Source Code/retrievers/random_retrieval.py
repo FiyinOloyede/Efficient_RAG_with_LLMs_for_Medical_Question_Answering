d7b7029ab6066ca07d07c0c5b8d589057d7b1b30
Ebenezer (Fiyinfoluwa) Oloyede <fiyinoloyede84@gmail.com>
2025-11-25 14:11:23 +0000

    Delete Source Code directory

--- Source Code/retrievers/random_retrieval.py
+++
@@ -1,9 +0,0 @@
-import random
-import pickle
-
-# Load pre-chunked documents
-with open("chunks.pkl", "rb") as f:
-    docs = pickle.load(f)
-
-def random_retriever(query: str, k: int = 5):
-    return random.sample(docs, k)
