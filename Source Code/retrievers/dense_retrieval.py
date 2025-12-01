62f181eb0da5d5ee56762e959379dfb34d27d3ab
Ebenezer (Fiyinfoluwa) Oloyede <fiyinoloyede84@gmail.com>
2025-12-01 15:31:30 +0000

    Update dense_retrieval.py

--- Source Code/retrievers/dense_retrieval.py
+++ Source Code/retrievers/dense_retrieval.py
@@ -3,11 +3,11 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 import pickle, os
 
-# Load pre-chunked documents from pickle
+# Load pre-chunked documents from pickle (external knowledge base)
 with open("chunks.pkl", "rb") as f:
     docs = pickle.load(f)
 
-# Load pre-chunked documents from pickle (knowledge base with dataset context)
+# Load pre-chunked documents from pickle (external knowledge base with dataset context)
 with open("chunks2.pkl", "rb") as f:
     docs_dcontext = pickle.load(f)
 
@@ -27,7 +27,7 @@ embedding_model_sbb = HuggingFaceEmbeddings(
 
 
 # Load (or build) FAISS vector stores
-vector_db_gtr = 'vector_store_gtr-t5-large'
+vector_db_gtr = 'vector_store_gtr-t5-large' # external Knowledge base 
 if os.path.exists(vector_db_gtr):
     # Load existing FAISS index
     index = FAISS.load_local(vector_db_gtr, 
@@ -39,11 +39,11 @@ else:
     index = FAISS.from_documents(docs, embedding_model_gtr)
     index.save_local(vector_db_gtr)
 
-# Create a retriever for dense embeddings
-dense_retriever_gtr = index.as_retriever(search_kwargs={"k": 5})
+# Create a retriever for dense embeddings (external Knowledge base)
+dense_retriever_gtr = index.as_retriever(search_kwargs={"k": 5}) 
 
 
-vector_db_gtr2 = 'vector_store_gtr-t5-large2' #Knowledge base build (with dataset contexts)
+vector_db_gtr2 = 'vector_store_gtr-t5-large2' # external Knowledge base (with dataset contexts)
 if os.path.exists(vector_db_gtr2):
     # Load existing FAISS index
     index = FAISS.load_local(vector_db_gtr2, 
@@ -55,11 +55,11 @@ else:
     index = FAISS.from_documents(docs_dcontext, embedding_model_gtr)
     index.save_local(vector_db_gtr2)
 
-# Create a retriever for dense embeddings
-dense_retriever_gtr2 = index.as_retriever(search_kwargs={"k": 5})
+# Create a retriever for dense embeddings (external Knowledge base with dataset contexts)
+dense_retriever_gtr2 = index.as_retriever(search_kwargs={"k": 5}) 
 
 
-vector_db_sbb = 'vector_store_pritamdeka'
+vector_db_sbb = 'vector_store_pritamdeka' # external Knowledge base
 if os.path.exists(vector_db_sbb):
     # Load existing FAISS index
     index = FAISS.load_local(vector_db_sbb, 
@@ -71,11 +71,11 @@ else:
     index = FAISS.from_documents(docs, embedding_model_sbb)
     index.save_local(vector_db_sbb)
 
-# Create a retriever for dense embeddings
+# Create a retriever for dense embeddings (external Knowledge base)
 dense_retriever_sbb = index.as_retriever(search_kwargs={"k": 5})
 
 
-vector_db_sbb2 = 'vector_store_pritamdeka2'  #Knowledge base build (with dataset contexts)
+vector_db_sbb2 = 'vector_store_pritamdeka2'  # external Knowledge base with dataset contexts
 if os.path.exists(vector_db_sbb2):
     # Load existing FAISS index
     index = FAISS.load_local(vector_db_sbb2, 
@@ -87,5 +87,5 @@ else:
     index = FAISS.from_documents(docs_dcontext, embedding_model_sbb)
     index.save_local(vector_db_sbb2)
 
-# Create a retriever for dense embeddings
+# Create a retriever for dense embeddings (external Knowledge base with dataset contexts)
 dense_retriever_sbb2 = index.as_retriever(search_kwargs={"k": 5})
