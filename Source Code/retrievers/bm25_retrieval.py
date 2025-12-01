ef5e3a4fa72e2f5d21507a4ca7009cb5c390a93f
Ebenezer (Fiyinfoluwa) Oloyede <fiyinoloyede84@gmail.com>
2025-12-01 15:18:37 +0000

    Update bm25_retrieval.py

--- Source Code/retrievers/bm25_retrieval.py
+++ Source Code/retrievers/bm25_retrieval.py
@@ -2,7 +2,7 @@
 from langchain_community.retrievers import BM25Retriever
 import pickle
 
-# Load pre-chunked documents from pickle
+# Load pre-chunked documents from pickle (external knowledge base)
 with open("chunks.pkl", "rb") as f:
     docs = pickle.load(f)
 
@@ -11,5 +11,6 @@ with open("chunks2.pkl", "rb") as f:
     docs_dcontext = pickle.load(f)
 
 # Initialize BM25 retrievers
-bm25_retriever = BM25Retriever.from_documents(docs, k=5)
-bm25_retriever2 = BM25Retriever.from_documents(docs_dcontext, k=5)
+bm25_retriever = BM25Retriever.from_documents(docs, k=5) # from external knowledge base
+bm25_retriever2 = BM25Retriever.from_documents(docs_dcontext, k=5) # from external knowledge base with dataset context
+
